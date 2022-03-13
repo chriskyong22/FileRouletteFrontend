001e9ffd1dadabcfcc77f44b1324ae4fe7ec9422
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2022-03-12 21:28:25 -0500

    Desired Response type is ByteArray to prevent data corrupt on binaries

--- src/Services/API.ts
+++ src/Services/API.ts
@@ -8,9 +8,20 @@ export const uploadFile = (file: File) => {
     axios.post(`${BASE_URL}upload/`, formData,  {
         headers: {
             'Content-Type': 'multipart/form-data'
-        }
+        }, 
+        responseType: 'arraybuffer'
     }).then((response) => {
-        console.log(response);
+        console.log(response.headers);
+        let contentDisposition = response.headers["content-disposition"];
+        let a = document.createElement('a');
+        console.log(response.headers['content-type']);
+        let data = new Blob([response.data], {type: response.headers['content-type']});
+        let url = window.URL.createObjectURL(data);
+        a.href = url;
+        let fileName = contentDisposition.split("filename=")[1];
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
     }).catch((error) => {
         console.error(error);
     })
